5efee8f9a240a1c75ec47a82a9f4af098d3e93df
sophie200 <44643926+sophie200@users.noreply.github.com>
2019-12-12 18:32:00 -0600

    ch6

--- crochetstore_project/settings.py
+++ crochetstore_project/settings.py
@@ -37,10 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third-party
+    'crispy_forms',
     #local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
+
+#django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,6 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # New stuff to help
 AUTH_USER_MODEL = 'users.CustomUser'
